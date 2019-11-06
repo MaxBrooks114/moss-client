06084d3ee20f481408747667015e96ad5bf89c54
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-05 21:30:34 -0500

    staged react app, connected redux, thunk, devtools, store

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  combineReducers
+} from 'redux'
+import thunk from 'redux-thunk'
+import users from './reducers/users'
+
+// just displaying different syntax options here (lines 8 and 9)
+const reducer = combineReducers({
+    users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
