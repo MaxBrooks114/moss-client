d15ff9d6b82d8b2eb3fa4afede21ce6072abe419
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:30:37 -0500

    pass userId to concertcard to change prezentational logic

--- src/components/ConcertCard.js
+++ src/components/ConcertCard.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import ReviewForm from './ReviewForm'
+import { connect } from 'react-redux'
 
-const ConcertCard = ({ concert }) => {
+const ConcertCard = ({ concert, userId }) => {
+console.log(userId)
+
   return (
+    concert ?
       <div>
-        <h3>{concert.name}</h3>
-        <p>{concert.date}</p>
-        <p>{concert.artist}</p>
-        <p>{concert.venue}</p>
-      </div>
+        <h3>{concert.attributes.name}</h3>
+        <p>Date: {concert.attributes.date}</p>
+        <p>Artist: {concert.attributes.artist}</p>
+        <p>Venue: {concert.attributes.venue}</p>
+        <p>{userId}</p>
+        <p>Aggregated Score:{concert.attributes.combined_review_score}</p>
+        {!!concert.attributes.reviews.find(rev => rev.user_id == userId) ? <p>final_score</p> : <Link to={`/concerts/${concert.id}/reviews/new`}>Add a review</Link> }
+
+
+      </div> : <div> Maaaaan they ain't no concert here </div>
   )
+
 }
 
-export default ConcertCard
+const mapStateToProps = state => {
+  const userId = state.currentUser ? state.currentUser.id : ""
+  return ({
+    userId,
+  })
+}
+
+
+export default connect(mapStateToProps)(ConcertCard)
