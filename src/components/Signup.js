d12e722f62dca1528af9e344aa7ada371dbb4d1c
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:35:07 -0500

    sign up form component

---
+++ src/components/Signup.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateSignup } from '../actions/signup.js'
+import { signup } from '../actions/currentUser.js'
+
+const Signup = ({ signupData, updateSignup, signup, history }) => {
+
+  const handleChange = event => {
+    const { name, value } = event.target
+    const updatedInfo = {
+      ...signupData,
+      [name]: value
+    }
+    updateSignup(updatedInfo)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    signup(signupData, history)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input placeholder="name" value={signupData.name} name="name" type="text" onChange={handleChange}/>
+      <input placeholder="username" value={signupData.username} name="username" type="text" onChange={handleChange}/>
+      <input placeholder="password" value={signupData.password} name="password" type="password" onChange={handleChange}/>
+      <input type ="submit" value = "Sign Up"/>
+    </form>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    signupData: state.signup
+  }
+}
+
+
+export default connect(mapStateToProps, { updateSignup, signup })(Signup)
