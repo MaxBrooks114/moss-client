ead08df702da00a9e71a2c3bb378b7f71dede0eb
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:33:11 -0500

    create form wrapper for new reviews to distinguish from edit review

---
+++ src/components/NewReviewFormWrapper.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReviewForm from './ReviewForm'
+import { createReview } from '../actions/review'
+import { connect } from 'react-redux'
+
+const NewReviewFormWrapper = ({ history, createReview }) => {
+
+  const handleSubmit = (formData, userId, concertId) => {
+    createReview({
+      ...formData,
+      userId, concertId
+    }, history)
+  }
+  return  <ReviewForm history={history} handleSubmit={handleSubmit} />
+};
+
+export default connect(null, { createReview })(NewReviewFormWrapper);
