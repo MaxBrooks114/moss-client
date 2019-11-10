fd46918b0892488e06e5ae9384bd44f2e3a4ab6f
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:31:15 -0500

    home component for sign in and log in

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './Login'
+import Logout from './Logout'
+import { Link } from 'react-router-dom'
+
+const Home = () => (
+  <div>
+   <h1>A rolling stone gathers no Moss</h1>
+    <span>
+      <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>
+    </span>
+  </div>
+);
+
+
+export default Home;
