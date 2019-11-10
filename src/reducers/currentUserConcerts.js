ec3c21d4620fb6367aa192aef7d0a82a1b7ac25a
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-10 10:34:09 -0500

    added intitial state to currentuserconcerts reducer to return intital

--- src/reducers/currentUserConcerts.js
+++ src/reducers/currentUserConcerts.js
@@ -1,10 +1,16 @@
+const initialState = []
 
-export default (state = [], action) => {
+
+
+export default (state = initialState, action) => {
 
   switch (action.type) {
 
     case "SET_CURRENT_USER_CONCERTS":
         return action.concerts
+
+    case "CLEAR_CURRENT_USER_CONCERTS":
+         return initialState
 
     default:
 
