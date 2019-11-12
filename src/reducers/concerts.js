ae71256b665b1ac3429c64feac73d25f12e6d055
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-11 21:33:40 -0500

    updated concert actions to get concerts

--- src/reducers/concerts.js
+++ src/reducers/concerts.js
@@ -2,18 +2,11 @@ const initialState = []
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "SET_CONCERTS":
-      return action.concerts
-    case "ADD_CONCERT":
-      return state.concat(action.concert)
-    case "UPDATE_CONCERT":
-      return state.map(concert => concert.id === action.concert.id ? action.concert : concert)
-    case "DELETE_CONCERT":
-      //console.log( "action.concertId is ", action.concertId)
-      return state.filter(concert => concert.id === action.concertId ? false : true)
-    case "CLEAR_CONCERTS":
-      return initialState
+    case 'LOADING_CONCERTS':
+      return state;
+    case 'GET_CONCERTS':
+      return action.concerts.data
     default:
-      return state
+      return state;
   }
 }
