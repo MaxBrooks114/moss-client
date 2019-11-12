0ba1de5362bc22137a64df4ab52618702eb65ac4
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-11 21:34:39 -0500

    cmobined user reviews and concerts reviews into reviews reducer

--- src/reducers/reviews.js
+++ src/reducers/reviews.js
@@ -2,14 +2,18 @@ const initialState = []
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case "SET_MY_REVIEWS":
+    case "SET_REVIEWS":
       return action.reviews
+    case "SET_USER_REVIEWS":
+      return action.reviews
+    case "SET_CONCERT_REVIEWS":
+        return action.reviews
     case "ADD_REVIEW":
       return state.concat(action.review)
     case "UPDATE_REVIEW":
       return state.map(review => review.id === action.review.id ? action.review : review)
     case "DELETE_REVIEW":
-      //console.log( "action.reviewId is ", action.reviewId)
+
       return state.filter(review => review.id === action.reviewId ? false : true)
     case "CLEAR_REVIEWS":
       return initialState
