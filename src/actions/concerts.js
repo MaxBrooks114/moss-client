377ac9001d09c9f8dc73a2e189fdc938de43443d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-13 17:20:36 -0500

    added get concerts and saveconcert actions

--- src/actions/concerts.js
+++ src/actions/concerts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import store from '../store';
 
 export const setConcerts = concerts => {
   return {
@@ -14,15 +14,54 @@ export const clearConcerts = () => {
   }
 }
 
-const REACT_APP_SEATGEEK_CLIENT_ID = process.env.REACT_APP_SEATGEEK_CLIENT_ID ;
+
+
+const REACT_APP_BANDSINTOWN_APP_ID = process.env.REACT_APP_BANDSINTOWN_APP_ID ;
 
 export const getConcerts = (query) => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_CONCERTS' });
-    return axios.get("https://rest.bandsintown.com/artists/Kishi%20Bashi/events?app_id=73b689174c8cd3f299a050e1c75f57a5&date=past")
-      .then(concerts => {dispatch({ type: 'GET_CONCERTS', concerts })}
+    return axios.get(`https://rest.bandsintown.com/artists/Kishi%20Bashi/events?app_id=${REACT_APP_BANDSINTOWN_APP_ID}&date=past`)
+      .then(concerts => {
+          dispatch({ type: 'GET_CONCERTS', concerts })
+          dispatch(addReviewsToConcerts(concerts))})
+    }
+    }
+
+export const saveConcert = (concertData) => {
+    return dispatch => {
+      const sendableConcertData = {
+        venue: concertData.venue.name,
+        artist: concertData.lineup[0],
+        date: concertData.datetime,
+        concert_api_id: parseInt(concertData.id, 10)
+      }
+      return fetch("http://localhost:3000/api/v1/concerts", {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(sendableConcertData)
+      })
+        .then(r => r.json())
+        .then(resp => {
+          if (resp.error) {
+            console.log(resp.error)
+          } else {
+            console.log(resp.data)
+
+          }
+        })
+        .catch(console.log)
+
+    }
+  }
 
 
-    ).then(concerts => {dispatch({ type: 'ADD_REVIEWS_TO_CONCERTS', concerts})})
-  };
+export const addReviewsToConcerts = (concerts) => {
+  return (dispatch, getState) => {
+    const reviews  = store.getState().reviews
+    return concerts.data.map(concert => concert.reviews = reviews.filter(review => review.concert.id === concert.id))
+}
 }
