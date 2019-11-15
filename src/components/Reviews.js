6fbfc0e36b10d2d1c7944bcd729a2292c0cd0436
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-15 09:45:50 -0500

    added functional reviews container with hooks

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ReviewsList from './ReviewsList'
+import { getReviews, getConcertReviews, getUserReviews } from '../actions/reviews'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
 
-
-
-
-const Reviews = props => {
-
-  const reviewCards = props.reviews.length > 0 ?
-    props.reviews.map(r => <><Link key={r.id} to={`/reviews/${r.id}`}>{r.attributes.concert.name} by {r.attributes.user.username}</Link><br/></>)  : <p>You have no reviews!</p>
-
-  return reviewCards
+const Reviews = ({getReviews, getUserReviews, getConcertReviews, concertId, currentUserId, userId}) => {
+  const [ reviews, setReviews ] = useState({})
+  useEffect(() => {
+      if (concertId){
+        getConcertReviews(concertId)
+      } else if (userId) {
+        getUserReviews(userId)
+      } else {
+        getReviews()
+      }
+    }, [concertId, getConcertReviews, getReviews, getUserReviews, userId])
+    return <ReviewsList reviews={reviews} />
 }
-
 
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    
-    reviews: state.reviews
-
-  }
+  const concertId = ownProps.match.path === "/concerts/:id/reviews" ? ownProps.match.params.id : null
+  const userId = ownProps.match.path === "/users/:id/reviews" ? ownProps.match.params.id : null
+  const currentUserId = state.currentUser ? state.currentUser.id : ""
+  return ({
+    concertId,
+    currentUserId,
+    userId
+  })
 }
 
-export default withRouter(connect(mapStateToProps)(Reviews))
+
+export default withRouter(connect(mapStateToProps, {getReviews,getUserReviews,getConcertReviews})(Reviews))
