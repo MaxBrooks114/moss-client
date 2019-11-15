763eb4e52a919fb253eda1ccdba9d8d6d5825e41
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-15 02:22:32 -0500

    import concertsearch, concert fetcher, removed old props functionality

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import { getReviews, getUserReviews} from './actions/reviews.js'
 import NavBar from './components/NavBar'
 import Login  from './components/Login'
 import Signup from './components/Signup'
-import Concerts from './components/Concerts'
+import ConcertsList from './components/ConcertsList'
 import CurrentUserConcerts  from './components/CurrentUserConcerts'
 import Home from './components/Home'
 import NewReviewFormWrapper from './components/NewReviewFormWrapper'
 import EditReviewFormWrapper from './components/EditReviewFormWrapper'
-import ConcertCard from './components/ConcertCard.js'
+import ConcertFetcher from './components/ConcertFetcher'
+import Concerts from './components/Concerts'
 import Reviews from './components/Reviews'
 import ReviewCard from './components/ReviewCard.js'
+import ConcertSearchForm from './components/ConcertSearchForm'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 
@@ -23,7 +25,6 @@ import { Route, Switch, withRouter } from 'react-router-dom'
 class App extends React.Component {
   componentDidMount(ownProps){
     this.props.getCurrentUser()
-    this.props.getConcerts()
 
   }
   render() {
@@ -31,18 +32,13 @@ class App extends React.Component {
     return (
       <div className="App">
         { loggedIn ? <NavBar/> : <Home/> }
+
         <Switch>
           <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
           <Route exact path='/login' component={Login}/>
           <Route exact path='/concerts' component={Concerts}/>
           <Route exact path='/users/:id/concerts' component={CurrentUserConcerts}/>
-          <Route exact path='/concerts/:id' render={props => {
-              const concert = concerts.find(concert => concert.id === props.match.params.id)
-              const userId = props.userId
-              console.log(concert)
-              return <ConcertCard concert={concert} userId={userId} {...props}/>
-            }
-          }/>
+          <Route exact path='/concerts/:id' component={ConcertFetcher}/>
           <Route exact path='/reviews/:id' render={props => {
               const review = reviews.find(review => review.id === props.match.params.id)
               const userId = props.userId
@@ -67,14 +63,12 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const concerts = ownProps.location.pathname.includes("users") ? state.currentUserConcerts : state.concerts
-  const reviews = state.reviews
   const userId = state.currentUser ? state.currentUser.id : ""
   return ({
     loggedIn: !!state.currentUser,
-    concerts,
+    concerts: state.concerts,
     userId,
-    reviews
+    reviews: state.reviews
   })
 
 }
