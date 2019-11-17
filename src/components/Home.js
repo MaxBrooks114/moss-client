f0520ee296e2b4d390cd143f9daccc5dc2ac970e
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-11-17 15:06:19 -0500

    put home in a jumbotron

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Jumbotron  from 'react-bootstrap/Jumbotron';
 
 const Home = () => (
-  <div>
-   <h1>A rolling stone gathers no Moss</h1>
+  <Jumbotron>
+    <h1>A rolling stone gathers no Moss</h1>
     <span>
       <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>
     </span>
-  </div>
+  </Jumbotron>
 );
 
 
